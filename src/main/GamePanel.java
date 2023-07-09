ab08051e3b592e38864084ccb5135d24f937d822
Dhruv <dhruv.punchcoory@umail.uom.ac.mu>
2023-07-09 21:11:27 +0400

    GamePanel class

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,6 +7,8 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import entity.Player;
+
 public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
@@ -24,6 +26,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     int playerX = 100;
     int playerY = 100;
@@ -71,7 +74,7 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-      player.update();
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
@@ -79,6 +82,7 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
+
         player.draw(g2);
         g2.dispose();
     }
